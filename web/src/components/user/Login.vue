d54b7235fe5ee5ae660d92a144a993b156a0e742
yz <1061604943@qq.com>
2018-05-09 11:31:46 +0800

    修改了登录界面

--- web/src/components/user/Login.vue
+++ web/src/components/user/Login.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="dialog">
+    <div class="head">
+      <h2 id="head">贵阳机场载重平衡基础数据支持系统</h2>
+    </div>
     <div class="loginPage">
-      <h1>登录</h1>
+      <h2>登录</h2>
       <el-form>
-        <el-form-item label="用户名">
+        <el-form-item label="帐号">
           <el-input type="text" id="user" v-model="formName.user" @blur="inputBlur('user',formName.user)"></el-input>
-          <p>{{formName.userError}}</p>
         </el-form-item>
         <el-form-item label="密码">
           <el-input type="password" id="password" v-model="formName.password" @blur="inputBlur('password',formName.password)"></el-input>
-          <p>{{formName.passwordError}}</p>
         </el-form-item>
         <el-button type="primary" @click="submitForm(formName)" v-bind:disabled="formName.beDisabled">提交</el-button>
         <el-button @click="resetForm">重置</el-button>
       </el-form>
+
     </div>
   </div>
 </template>
 
 <style scoped>
+  #head {
+    position: absolute;
+    left: 70%;
+  }
+  .dialog {
+    position: absolute;
+    width: 1360px;
+    height: 654px;
+    background-image: url("../../assets/IMG_1443_01.jpg");
+  }
   .loginPage{
     position: absolute;
-    top: 40%;
-    left: 50%;
+    top: 60%;
+    left: 85%;
     margin-top: -150px;
     margin-left: -175px;
     width: 350px;
@@ -31,7 +43,7 @@
     border-radius: 8px;
     box-sizing: border-box;
     /*background-color: #fff;*/
-    background-image: url("../../assets/test.jpg");
+    /*background-image: url("../../assets/1443.jpg");*/
   }
   .loginPage p{
     color: red;
@@ -41,7 +53,10 @@
 
 <script>
 
+  import ElAlert from "../../../node_modules/element-ui/packages/alert/src/main.vue";
+
   export default {
+    components: {ElAlert},
     name: '',
     data () {
       return {
@@ -99,13 +114,15 @@
       inputBlur:function(errorItem,inputContent){
         if (errorItem === 'user') {
           if (inputContent === '') {
-            this.formName.userError = '用户名不能为空'
+            this.formName.userError = '用户名不能为空';
+            this.$message.error(this.formName.userError)
           }else{
             this.formName.userError = '';
           }
         }else if(errorItem === 'password') {
           if (inputContent === '') {
-            this.formName.passwordError = '密码不能为空'
+            this.formName.passwordError = '密码不能为空';
+            this.$message.error( this.formName.passwordError)
           }else{
             this.formName.passwordError = '';
           }
